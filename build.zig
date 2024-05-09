f507f394a084f0a2374f55b4dca88ed08e1032f4
Frank Denis <github@pureftpd.org>
2024-05-09 21:44:35 +0200

    up

--- build.zig
+++ build.zig
@@ -9,9 +9,9 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .strip = true,
     });
-    lib.addCSourceFile(.{ .file = .{ .path = "src/minisign_verify.c" } });
-    lib.addIncludePath(.{ .path = "/opt/homebrew/include" });
-    lib.addLibraryPath(.{ .path = "/opt/homebrew/lib" });
+    lib.addCSourceFile(.{ .file = b.path("src/minisign_verify.c") });
+    lib.addIncludePath(.{ .cwd_relative = "/opt/homebrew/include" });
+    lib.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/lib" });
     lib.linkSystemLibrary("sodium");
     b.installArtifact(lib);
 }
