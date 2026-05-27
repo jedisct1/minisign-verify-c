9e9d6c08fb55b6f41f0757a343e8f7417bff7a8a
Frank Denis <github@pureftpd.org>
2026-05-27 14:16:11 +0200

    Update for zig-current

--- build.zig
+++ build.zig
@@ -3,15 +3,22 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const lib = b.addStaticLibrary(.{
-        .name = "minisign_verify",
+
+    const lib_mod = b.createModule(.{
         .target = target,
         .optimize = optimize,
         .strip = true,
+        .link_libc = true,
     });
-    lib.addCSourceFile(.{ .file = b.path("src/minisign_verify.c") });
-    lib.addIncludePath(.{ .cwd_relative = "/opt/homebrew/include" });
-    lib.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/lib" });
-    lib.linkSystemLibrary("sodium");
+    lib_mod.addCSourceFile(.{ .file = b.path("src/minisign_verify.c") });
+    lib_mod.addIncludePath(.{ .cwd_relative = "/opt/homebrew/include" });
+    lib_mod.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/lib" });
+    lib_mod.linkSystemLibrary("sodium", .{});
+
+    const lib = b.addLibrary(.{
+        .name = "minisign_verify",
+        .root_module = lib_mod,
+        .linkage = .static,
+    });
     b.installArtifact(lib);
 }
